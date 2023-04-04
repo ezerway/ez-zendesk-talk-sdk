640043173dc9cda651c52a6419cbd6ec1293667f
ezerway <54234094+ezerway@users.noreply.github.com>
2023-04-04 20:47:30 +0700

    Up to v0.1.3

--- android/src/main/java/expo/modules/ezzendesktalksdk/LineStatusChecker.kt
+++ android/src/main/java/expo/modules/ezzendesktalksdk/LineStatusChecker.kt
@@ -5,7 +5,7 @@ import zendesk.talk.android.LineStatusResult
 class LineStatusChecker {
 
     suspend fun lineStatus(digitalLine: String): Boolean {
-        return when (val lineStatus = ExpoZendeskTalkModule.talk?.lineStatus(digitalLine)) {
+        return when (val lineStatus = EzZendeskTalkSdkModule.talk?.lineStatus(digitalLine)) {
             is LineStatusResult.Success -> lineStatus.agentAvailable
             is LineStatusResult.Failure -> false
             null -> false
